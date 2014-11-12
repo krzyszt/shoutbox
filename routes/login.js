4260f723b460932d6730f72a475cfc72602e5ff1
Krzysztof Kaminski <krzysztof.teonte@gmail.com>
2014-11-12 13:56:27 +0000

    login functionality

---
+++ routes/login.js
@@ -0,0 +1,31 @@
+var User = require('../lib/user');
+
+module.exports.form = function(req,res){
+  res.render('login', { title: 'Login' });
+};
+
+module.exports.submit = function(req,res, next){
+   var data = req.body.user;
+   User.authenticate(data.name, data.pass, function(err, user){
+      if (err) {
+         return next(err);
+      }
+      if(user){
+         req.session.uid = user.id;
+         res.redirect('/');
+      } else {
+         res.error("Sorry! Invalid credentials.");
+         res.redirect('back');
+      }
+   });
+};
+
+module.exports.logout = function(req,res){
+   req.session.destroy(function(err){
+      if (err) {
+         throw err;
+      }
+      res.redirect('/');
+   });
+   
+};
