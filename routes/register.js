ef5dac39cac0d8dd778aae8cbc667f4afbbfba0e
Krzysztof Kaminski <krzysztof.teonte@gmail.com>
2014-11-12 11:50:05 +0000

    submit/register

--- routes/register.js
+++ routes/register.js
@@ -1,8 +1,34 @@
+var User = require('../lib/user');
+
 module.exports.form = function(req,res){
 	res.render('register', { title: 'Register'});
-}
+};
 
 
-module.exports.submit = function(req,res){
-	
-}
+module.exports.submit = function(req,res, next){
+	var data = req.body.user;
+    
+    User.getByName(data.name, function(err, user){
+       if (err) { return next(err); }
+       
+       if (user.id){
+          res.error('User already taken!');
+          res.redirect('back');
+       } else {
+          user = new User({
+             name: user.name,
+             pass: data.pass
+          });
+          
+          user.save(function(err){
+             if (err) {
+                return next(err);
+             }
+             req.session.uid = user.id;
+             res.redirect('/');
+          });
+       }
+       
+    });
+    
+};
