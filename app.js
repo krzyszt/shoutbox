574948c3c6fff33210405a4653ab382840a88d98
Krzysztof Kaminski <krzysztof.teonte@gmail.com>
2014-11-12 10:49:16 +0000

    messages support

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var register = require('./routes/register');
 
 var app = express();
 
@@ -23,6 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.get('/register', register.form);
+app.post('/register', register.submit);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/register.js
@@ -0,0 +1,8 @@
+module.exports.form = function(req,res){
+	res.render('register', { title: 'Register'});
+}
+
+
+module.exports.submit = function(req,res){
+	
+}
